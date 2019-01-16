8270d33108020b8909fd67c3e68f4d4e66f64ec6
Jamie <mailtojamie@163.com>
2019-01-16 14:39:33 +0800

    Update pytorch_sample_std_mean.py

--- pytorch_sample_std_mean.py
+++ pytorch_sample_std_mean.py
@@ -2,10 +2,10 @@ import numpy as np
 import cv2,os
 import random
  
-# calculate means and std
-train_txt_path = './test_hp_new_0104.txt'
+# calculate means and std for your data
+train_txt_path = './train_val.txt'
  
-CNum = 10000     # 挑选多少图片进行计算
+CNum = 10000     # pick some to cal
  
 img_h, img_w = 32, 32
 imgs = np.zeros([img_w, img_h, 3, 1])
@@ -14,11 +14,10 @@ means, stdevs = [], []
 with open(train_txt_path, 'r') as f:
     lines = f.readlines()
     lines = [line.split()[0] for line in lines]
-    random.shuffle(lines)   # shuffle , 随机挑选图片
+    random.shuffle(lines)   # shuffle to pick pictures
  
     for i in (range(len(lines))):
-        img_path = os.path.join('/home/creatorai/workspace/1016_detect/crnn.pytorch/data/labelimageTest20190103/', lines[i].rstrip().split()[0])
- 
+        img_path = os.path.join('/', lines[i].rstrip().split()[0]) 
         img = cv2.imread(img_path)
         img = cv2.resize(img, (img_h, img_w))
         img = img[:, :, :, np.newaxis]
@@ -29,11 +28,11 @@ with open(train_txt_path, 'r') as f:
 imgs = imgs.astype(np.float32)/255.
 
 for i in (range(3)):
-    pixels = imgs[:,:,i,:].ravel()  # 拉成一行
+    pixels = imgs[:,:,i,:].ravel()  # flatten to line
     means.append(np.mean(pixels))
     stdevs.append(np.std(pixels))
                   
-# cv2 读取的图像格式为BGR，PIL/Skimage读取到的都是RGB不用转
+# cv2 BGR，PIL/Skimage RGB No Need To Trans
 means.reverse() # BGR --> RGB
 stdevs.reverse()
  
